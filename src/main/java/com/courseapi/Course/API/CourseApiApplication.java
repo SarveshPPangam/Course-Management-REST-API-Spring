0e44f329f9eb016f4c91dc8b5f53cea7cd3f6eb5
SarveshPPangam <81740597+SarveshPPangam@users.noreply.github.com>
2021-07-02 13:15:28 +0530

    Update CourseApiApplication.java

--- src/main/java/com/courseapi/Course/API/CourseApiApplication.java
+++ src/main/java/com/courseapi/Course/API/CourseApiApplication.java
@@ -33,40 +33,5 @@ public class CourseApiApplication {
 		SpringApplication.run(CourseApiApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner commandLineRunner(){
-		return args -> {
-//			Course course = new Course();
-//			course.setName("Machine Learning");
-//			course.setCreatedAt(new Timestamp(System.currentTimeMillis()));
-//			Student student = new Student();
-			Optional<Course> course = courseRepository.findById(2);
-//			student.setCreatedAt(new Timestamp(System.currentTimeMillis()));
-//			student.setFirstName("f");
-//			student.setLastName("l");
-//			student.addCourse(course.get());
-			Teacher teacher = new Teacher();
-//			teacher.setFirstName("tf");
-//			teacher.setLastName("tl");
-//			teacher.addCourse(course.get());
-//			teacher.addStudents(course.get().getStudents());
-//
-//			Optional<Teacher> teacher1 = teacherRepository.findById(2);
-//
-//			Student student = studentService.findById(4);
-//			System.out.println(student.getCourses());
-//			student.unEnroll(course.get());
-//			studentService.save(student);
-//			courseRepository.save(course.get());
-//			teacherRepository.save(teacher1.get());
-//			//student.enroll(course.get());
-//			System.out.println("stu"+student.getCourses());
-//			System.out.println("stu"+student.getTeachers());
-//			System.out.println("cour"+course.get().getStudents());
-//			System.out.println("tea"+teacher1.get().getStudents());
-			//teacherRepository.save(teacher);
-			//studentService.save(student);
-		};
-	}
 
 }
