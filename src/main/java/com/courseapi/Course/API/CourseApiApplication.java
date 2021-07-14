29a0edd51edb1b617c54ad104702438854088bbc
SarveshPPangam <81740597+SarveshPPangam@users.noreply.github.com>
2021-07-14 19:59:46 +0530

    Update CourseApiApplication.java

--- src/main/java/com/courseapi/Course/API/CourseApiApplication.java
+++ src/main/java/com/courseapi/Course/API/CourseApiApplication.java
@@ -1,13 +1,5 @@
 package com.courseapi.Course.API;
 
-import com.courseapi.Course.API.models.Course;
-import com.courseapi.Course.API.models.Student;
-import com.courseapi.Course.API.models.Teacher;
-import com.courseapi.Course.API.repositories.CourseRepository;
-import com.courseapi.Course.API.repositories.StudentRepository;
-import com.courseapi.Course.API.repositories.TeacherRepository;
-import com.courseapi.Course.API.services.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,12 +14,6 @@ import java.util.jar.JarOutputStream;
 @SpringBootApplication
 public class CourseApiApplication {
 
-	@Autowired
-	StudentService studentService;
-	@Autowired
-	CourseRepository courseRepository;
-	@Autowired
-	TeacherRepository teacherRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApplication.class, args);
